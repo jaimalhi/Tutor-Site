43476b64ccc25a9e404447ab94819339b372cfc5
Jai <malhi.jaivir@gmail.com>
2024-06-13 16:02:55 -0700

    added footer

---
+++ frontend/src/lib/components/layout/Footer.svelte
@@ -0,0 +1,34 @@
+<footer class="footer p-10 bg-neutral text-base-100">
+   <aside>
+      <img src="/avio-light.png" width="128px" alt="avio-tutor-logo" />
+   </aside>
+   <nav>
+      <h6 class="footer-title">About Us</h6>
+      <a href="/" class="link link-hover">Terms of Service</a>
+      <a href="/" class="link link-hover">Privacy Policy</a>
+      <a href="/" class="link link-hover">Become a Tutor</a>
+   </nav>
+   <nav>
+      <h6 class="footer-title">Contact</h6>
+      <a class="link link-hover" id="icons">
+         <img src="/icons/mail.png" alt="email-icon" />
+         aviotutor@gmail.com
+      </a>
+      <a class="link link-hover" id="icons">
+         <img src="/icons/telephone.png" alt="email-icon" />
+         +1 775 989 0044
+      </a>
+   </nav>
+</footer>
+
+<style>
+   #icons img {
+      filter: invert(1) brightness(2);
+      margin-right: 8px;
+      width: 20px;
+   }
+
+   #icons {
+      display: flex;
+   }
+</style>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,9 @@
    import "../global.css";
    import "tailwindcss/tailwind.css";
    import Navbar from "$lib/components/layout/Navbar.svelte";
+   import Footer from "$lib/components/layout/Footer.svelte";
 </script>
 
 <Navbar />
 <slot />
+<Footer />
