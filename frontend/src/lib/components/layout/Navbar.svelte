34aaabb6ad13080d74c803a3bbadb8daa0271d6f
Jai <malhi.jaivir@gmail.com>
2024-06-16 22:08:39 -0700

    TEST: Bug-fix attept - navbar dropdown button clickable on mobile devices

--- frontend/src/lib/components/layout/Navbar.svelte
+++ frontend/src/lib/components/layout/Navbar.svelte
@@ -1,11 +1,18 @@
 <script>
    import { calendlyLink } from "$lib/config/links.js";
+
+   let dropdownOpen = false;
+
+   function toggleDropdown() {
+      dropdownOpen = !dropdownOpen;
+   }
 </script>
 
-<div class="navbar absolute bg-neutral-content md:bg-transparent z-[10]">
+<div class="navbar absolute bg-neutral-content md:bg-transparent z-10">
    <div class="navbar-start mt-1">
-      <a href="/" class="flex justify-end"
-         ><img src="/logos/avio-light.png" width="128px" alt="avio-tutor-logo" /></a>
+      <a href="/" class="flex justify-end">
+         <img src="/logos/avio-light.png" width="128px" alt="avio-tutor-logo" />
+      </a>
    </div>
    <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1 font-semibold text-base-100">
@@ -15,47 +22,51 @@
       </ul>
    </div>
    <div class="navbar-end">
-      <div class="dropdown dropdown-left">
+      <button class="dropdown dropdown-left" on:click={toggleDropdown}>
          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
             <svg
                xmlns="http://www.w3.org/2000/svg"
                class="h-5 w-5 text-base-100"
                fill="none"
                viewBox="0 0 24 24"
-               stroke="currentColor"
-               ><path
+               stroke="currentColor">
+               <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                  d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
          </div>
-         <ul
-            class="menu menu-sm dropdown-content mt-3 z-[11] p-2 shadow bg-base-100 rounded-box w-52 font-semibold"
-            id="nav-dropdown">
-            <li><a href="/" on:click={() => (window.location.href = "/")}>Home</a></li>
-            <li>
-               <a href="/how-it-works" on:click={() => (window.location.href = "how-it-works")}
-                  >How it works</a>
-            </li>
-            <li>
-               <a href="/contact" on:click={() => (window.location.href = "/contact")}
-                  >Contact Us</a>
-            </li>
-            <hr />
-            <!-- <li><a href="/login" class="text-primary">Login</a></li> -->
-            <li>
-               <a
-                  href={calendlyLink}
-                  target="_blank"
-                  class="text-primary"
-                  on:click={() => (window.location.href = calendlyLink)}>FREE Consultation</a>
-            </li>
-         </ul>
-      </div>
+         {#if dropdownOpen}
+            <ul
+               class="menu menu-sm dropdown-content mt-3 z-[11] p-2 shadow bg-base-100 rounded-box w-52 font-semibold"
+               id="nav-dropdown">
+               <li><a href="/">Home</a></li>
+               <li>
+                  <a href="/how-it-works">How it works</a>
+               </li>
+               <li>
+                  <a href="/contact">Contact Us</a>
+               </li>
+               <hr />
+               <!-- <li><a href="/login" class="text-primary">Login</a></li> -->
+               <li>
+                  <a href={calendlyLink} target="_blank" class="text-primary">
+                     FREE Consultation
+                  </a>
+               </li>
+            </ul>
+         {/if}
+      </button>
       <div class="hidden lg:flex">
          <!-- <a href="/login" class="btn btn-outline text-base-100 mr-4 px-6">Login</a> -->
-         <a href={calendlyLink} target="_blank" class="btn btn-primary text-base-100 mr-4"
-            >Book a FREE Consultation</a>
+         <a
+            href={calendlyLink}
+            target="_blank"
+            class="btn btn-primary text-base-100 mr-4"
+            on:click={() => (window.location.href = calendlyLink)}>
+            Book a FREE Consultation
+         </a>
       </div>
    </div>
 </div>
