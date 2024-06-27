c5578ccf26c48d6ed4fc851f2c5aa79fd87ccdf6
Jai <malhi.jaivir@gmail.com>
2024-06-26 20:53:11 -0700

    Added SEO content to head

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -7,12 +7,15 @@
 
 <svelte:head>
    <title>Avio Tutor</title>
-   <meta property="title" content="Avio Tutor - Personalized 1-on-1 Tutoring" />
    <meta
       name="description"
       content="Avio Tutor provides personalized one-on-one tutoring for students of all ages. We offer flexible scheduling with virtual sessions. Our goal is to boost confidence, enhance critical thinking skills, and help students achieve academic success." />
+   <meta
+      name="keywords"
+      content="tutoring, personalized tutoring, 1-on-1 tutoring, virtual tutoring, academic success, student tutoring" />
    <link rel="canonical" href="https://www.aviotutor.com" />
    <meta name="author" content="Avio Tutor" />
+   <!-- Open Graph metadata -->
    <meta property="og:title" content="Avio Tutor - Personalized 1-on-1 Tutoring" />
    <meta
       property="og:description"
@@ -20,6 +23,7 @@
    <meta property="og:url" content="https://www.aviotutor.com" />
    <meta property="og:site_name" content="Avio Tutor" />
    <meta property="og:type" content="website" />
+   <meta name="robots" content="index, follow" />
 </svelte:head>
 
 <Navbar />
